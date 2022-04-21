90e0edbf33ad303bfc297da275c6ac015d83ec82
rohitkandiyal <kandiyalrohit@gmail.com>
2022-04-21 15:40:10 +0530

    S3 bucket TF

--- TF/cloudfront-S3/main.tf
+++ TF/cloudfront-S3/main.tf
@@ -6,31 +6,30 @@ terraform {
   required_version = "~> 0.14.3"
   backend "s3" {
     encrypt  = "true"
-    bucket   = "business-service-integration-terraform-backends"
+    bucket   = "smp-terraform-modules-examples"
     region   = "eu-central-1"
-    key      = "cloudfront-s3-cdn/safeatwork/terraform.tfstate"
+    key      = "cloudfront-s3-cdn/test-perm/terraform.tfstate"
   }
 
   required_providers {}
 }
 
 module "cdn" {
-  source                    = "git::https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn.git?ref=0.40.0"
+  source                    = "git::https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn.git?ref=0.82.4"
   namespace                 = "smp"
-  name                      = "safe-at-work-pwa"
-  environment               = "integ"
-  stage                     = ""
-  label_order               = ["namespace", "name", "environment", "stage"]
-  aliases                   = ["safeatwork-pwa.integration.smp.bcg.com"]
-  parent_zone_id            = "Z1ZMS3NQN36LGI"
+  name                      = "crown-jewel-test"
+  environment               = "sandbox"
+  label_order               = ["namespace", "name", "environment"]
+  aliases                   = ["safeatwork-pwa.tooling.test.smp-paas.com"]
+  parent_zone_id            = "ZZN2DM19IQLNS"
   dns_alias_enabled         = true
   web_acl_id                = ""
-  acm_certificate_arn       = "arn:aws:acm:us-east-1:068874145936:certificate/006f829d-21b1-4b59-b548-61d3f7e5ee12"
+  acm_certificate_arn       = "arn:aws:acm:eu-central-1:257652058521:certificate/395a8128-8e47-461d-acf5-9ae96e65bdda"
   allowed_methods           = ["GET", "HEAD", "OPTIONS"] #["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
   cached_methods            = ["GET", "HEAD", "OPTIONS"]
   cors_allowed_headers      = ["*"]
   cors_allowed_methods      = ["GET", "HEAD"]
-  cors_allowed_origins      = ["*.integration.smp.bcg.com"]
+  cors_allowed_origins      = ["*.tooling.test.smp-paas.com"]
   cors_expose_headers       = ["ETag"]
   encryption_enabled        = true
   forward_header_values     = ["Access-Control-Request-Headers", "Access-Control-Request-Method", "Access-Control-Allow-Origin", "Origin"]
